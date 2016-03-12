bdb7dd7d56dce5606708313527aa52e865b048de
Scott Brenstuhl <brenstsr@miamioh.edu>
2016-03-11 19:27:18 -0800

    create samples of corpus data

--- exploritoty.R
+++ exploritoty.R
@@ -1,16 +1,35 @@
-#setwd('Box Sync/datasciencecoursera/Capstone/')
+setwd('Box Sync/datasciencecoursera/Capstone/')
 
 library('tm')
 library('readr')
 library('SnowballC')
 
-directory <- DirSource('final/en_US/')
-testCorp <- Corpus(directory, readerControl = list(language="en"))
+directory <- DirSource('test/')
+sample.corp <- Corpus(directory, readerControl = list(language="en"))
 
-testCorp$en_US.twitter.txt
+summary(sample.corp)  
+sample.corp <- tm_map(sample.corp, removeNumbers)
+sample.corp <- tm_map(sample.corp, removePunctuation)
+sample.corp <- tm_map(sample.corp, stripWhitespace)
+sample.corp <- tm_map(sample.corp, tolower)
+sample.corp <- tm_map(sample.corp, removeWords, stopwords("english"))
+sample.corp <- tm_map(sample.corp, stemDocument, language = "english")
+sample.corp <- tm_map(sample.corp, PlainTextDocument)
+sample.dtm <- DocumentTermMatrix(sample.corp) 
 
-testnumrem<- tm_map(testCorp, removeNumbers)
-testMatrix <- DocumentTermMatrix(testnumrem)
+inspect(sample.dtm)
 
-directory2 <- DirSource('final/en_US/en_US.news.txt')
-testCorp2 <- Corpus(directory2, readerControl = list(language="en"))
+freq <- colSums(as.matrix(sample.dtm)) 
+
+length(freq)
+
+ord <- order(freq)
+
+dtms <- removeSparseTerms(sample.dtm, 0.10)
+View(inspect(dtms))
+
+freq[tail(ord,100)]
+
+findFreqTerms(sample.dtm, lowfreq=500)
+
+
---
+++ make_sample.R
@@ -0,0 +1,21 @@
+library(readr)
+
+# Read everything in from the file provided on Coursera
+en_twit <- read_table('final/en_US/en_US.twitter.txt', col_names = FALSE)
+en_blog <- read_table('final/en_US/en_US.blogs.txt', col_names = FALSE)
+en_news <- read_table('final/en_US/en_US.news.txt', col_names = FALSE)
+
+# Sample each of them
+twit_sample <- en_twit[sample(nrow(en_twit), round(nrow(en_twit)*.01)),]
+blog_sample <- en_blog[sample(nrow(en_blog), round(nrow(en_blog)*.01)),]
+news_sample <- en_news[sample(nrow(en_news), round(nrow(en_news)*.01)),]
+
+# Write test files to test dir so easy to build Corpus
+write(twit_sample,'test/twitter_sample.txt')
+write(blog_sample, 'test/blog_sample.txt')
+write(news_sample, 'test/news_sample.txt')
+
+# If using interactively, uncomment below to drop the big files from env:
+# rm(en_blog)
+# rm(en_news)
+# rm(en_twit)
